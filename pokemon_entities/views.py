809aaa2360bd1b85881a16ce0bdb1234814ed3a3
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-06-30 22:46:08 +0500

    Show pokemons on main page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,9 @@
 import folium
 import json
 
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpRequest
 from django.shortcuts import render
+from pokemon_entities.models import PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -27,24 +28,24 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+    #     pokemons = json.load(database)['pokemons']
+    pokemons = PokemonEntity.objects.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
-
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon_entity in pokemons:
+        # pokemon_image_url = pokemon_entity.pokemon.image.url
+        pokemon_image_url = request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+        add_pokemon(
+            folium_map, pokemon_entity.latitude,
+            pokemon_entity.longitude,
+            pokemon_image_url
+        )
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon_entity.id,
+            'img_url': pokemon_image_url,
+            'title_ru': pokemon_entity.pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
